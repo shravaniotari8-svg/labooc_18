31dd527c9f5a642b6ec0e1ba9d1d36421987c4c5
shravaniotari8-svg <shravaniotari8@gmail.com>
2026-07-24 14:46:18 +0000

    namespace use

---
+++ adsb.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int add()
+{
+    int a,b,add;
+    cout<<"enter two numbers=";
+    cin>>a>>b;
+    add=a+b;
+    return add;
+}
+
+int sub()
+{
+    int x,y,sub;
+    cout<<"enter two numbers=";
+    cin>>x>>y;
+    sub=x-y;
+    return sub;
+}
---
+++ file_2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+namespace syc
+{
+    int show();
+}
+int main()
+{
+    syc::show();
+    return 0;
+}
